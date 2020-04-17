555ac8398dc43edcbba5a2e48f24454cdd673d32
buskerbusker <wannamakeeasy@gmail.com>
2020-04-17 17:45:37 +0900

    AddQna

---
+++ src/main/java/com/iu/s5/qna/QnaController.java
@@ -0,0 +1,20 @@
+package com.iu.s5.qna;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/qna/**")
+public class QnaController {
+
+	@Autowired
+	private QnaService qnaService;
+
+	@ModelAttribute("board")
+	public String getBoard() throws Exception {
+		return "qna";
+	}
+
+}
---
+++ src/main/java/com/iu/s5/qna/QnaService.java
@@ -0,0 +1,47 @@
+package com.iu.s5.qna;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iu.s5.board.BoardService;
+import com.iu.s5.board.BoardVO;
+
+@Service
+public class QnaService implements BoardService {
+
+	@Autowired
+	private QnaDAO qnaDAO;
+
+	@Override
+	public List<BoardVO> boardList(int curPage) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardVO boardSelect(long num) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int boardWrite(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int boardUpdate(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int boardDelete(long num) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
